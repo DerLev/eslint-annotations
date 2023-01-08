330df794370a7d8ccb0b35cdcf1eef9859e9215f
DerLev <levin.schroeren@gmail.com>
2023-01-08 11:02:23 +0100

    testing exec

--- src/action.ts
+++ src/action.ts
@@ -1,4 +1,5 @@
 import * as core from '@actions/core'
+import exec from '@actions/exec'
 import * as fs from 'fs/promises'
 import path from 'path'
 
@@ -11,18 +12,29 @@ const typescriptAnnotations = async (inputFile: string) => {
 }
 
 (async () => {
-  const eslintInput = core.getInput('eslint-report')
-  const typescriptInput = core.getInput('typescript-log')
+  const eslintInput = process.env.NODE_ENV === 'development' ?
+    'eslint_report.json' :
+    core.getInput('eslint-report')
+  const typescriptInput = process.env.NODE_ENV === 'development' ?
+    'typescript.log' :
+    core.getInput('typescript-log')
   const errorOnWarn = Boolean(core.getInput('error-on-warn'))
+
+  const pwd = await exec.exec('pwd')
+  console.log(pwd)
 
   try {
     if(eslintInput) {
+      core.startGroup('eslint annotations')
       const eslintFile: EslinJsonOutput[] = await JSON.parse(await (await fs.readFile(path.join('./', eslintInput))).toString())
       await eslintAnnotations(eslintFile)
+      core.endGroup()
     }
     if(typescriptInput) {
+      core.startGroup('typescript annotations')
       const typescriptFile = await (await fs.readFile(path.join('./', typescriptInput))).toString()
       await typescriptAnnotations(typescriptFile)
+      core.endGroup()
     }
   } catch (err) {
     core.error(String(err), { title: 'Error reading file' })
