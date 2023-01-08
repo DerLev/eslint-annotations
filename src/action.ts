224409c44b8f1fd893ee81ee7624e17258723688
DerLev <levin.schroeren@gmail.com>
2023-01-08 10:49:59 +0100

    adding seperate functions for ts and es checking

--- src/action.ts
+++ src/action.ts
@@ -2,6 +2,14 @@ import * as core from '@actions/core'
 import * as fs from 'fs/promises'
 import path from 'path'
 
+const eslintAnnotations = async (inputFile: EslinJsonOutput[]) => {
+  console.log(inputFile)
+}
+
+const typescriptAnnotations = async (inputFile: string) => {
+  console.log(inputFile)
+}
+
 (async () => {
   const eslintInput = core.getInput('eslint-report')
   const typescriptInput = core.getInput('typescript-log')
@@ -9,12 +17,12 @@ import path from 'path'
 
   try {
     if(eslintInput) {
-      const eslintFile = await JSON.parse(await (await fs.readFile(path.join('./', eslintInput))).toString())
-      console.log(eslintFile)
+      const eslintFile: EslinJsonOutput[] = await JSON.parse(await (await fs.readFile(path.join('./', eslintInput))).toString())
+      await eslintAnnotations(eslintFile)
     }
     if(typescriptInput) {
       const typescriptFile = await (await fs.readFile(path.join('./', typescriptInput))).toString()
-      console.log(typescriptFile)
+      await typescriptAnnotations(typescriptFile)
     }
   } catch (err) {
     core.error(String(err), { title: 'Error reading file' })
